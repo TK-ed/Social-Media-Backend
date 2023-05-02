9e0974b0f0eabfc518dbedecd9b690fb9c173425
R1ck_S4ncherz <77538493+vineeth-krishna-1234@users.noreply.github.com>
2023-05-02 22:21:25 +0530

    fix(signup):de-bugged errors

--- Server/server.js
+++ Server/server.js
@@ -1,43 +1,39 @@
 // importing libraries
-import * as dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
-const app = express()
+const app = express();
 
 // importing files
-import router from './routes/user.js'
+import router from "./routes/user.js";
 
 // dotenv variables
-const port = process.env.PORT
-const db = process.env.MONG_URI
+const port = process.env.PORT;
+const db = process.env.MONG_URI;
 
 // middlewares
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/api', router)
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use("/api", router);
 
 // routes
-app.get('/', (req, res) => {
-    res.status(200).send('Haiiiyyaaa!!')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Haiiiyyaaa!!");
+});
 
 // port n db connection
-mongoose.connect(db)
-    .then( () => {
-        try {
-        
-            // connecting port
-            app.listen(port, () => {
-                console.log(`Connected to DB n listenin' on port ${port}`)
-            })
-
-        } catch(err) {
-            console.log(`Uh-Oh something's wrong!!`)
-        }
-        
-    })
-
+mongoose.connect(db).then(() => {
+  try {
+    // connecting port
+    app.listen(port, () => {
+      console.log(`Connected to DB n listenin' on port ${port}`);
+    });
+  } catch (err) {
+    console.log(`Uh-Oh something's wrong!!`);
+  }
+});
